3dd942b759f2580db639babdbedf657d058a1f1c
redfenix <alexanderkiryshin@gmail.com>
2019-05-27 00:11:17 +0300

    final commit

--- Assets/Scripts/Constants/Prices.cs
+++ Assets/Scripts/Constants/Prices.cs
@@ -1,7 +1,7 @@
 ﻿
 public class Prices
 {
-    public const float OIL_RIG_IN_OIL_PRICE = 25f;
+    public const float OIL_RIG_IN_OIL_PRICE = 20f;
     public const float GUARD_TOWER_PRICE = 60f;
     public const float AMOUNT_OIL_PRODUCING = 10;
 }
